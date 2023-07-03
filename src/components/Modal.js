1efb48552a461d277bd51de09b1b2539fd94efba
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-02 21:08:00 -0500

    adds to modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,5 +2,15 @@ import React, { useState } from "react";
 
 
 function Modal = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openModal = () => {
+        setIsOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
     
 }
